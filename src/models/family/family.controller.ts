d7c21cd55d0910a5f1ea39589a246abaf20495c5
khoavn02 <khoavungoc02@gmail.com>
2024-04-21 23:09:35 +0700

    update family controller

--- src/models/family/family.controller.ts
+++ src/models/family/family.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { FamilyService } from './family.service';
 
 @Controller({
@@ -7,4 +7,19 @@ import { FamilyService } from './family.service';
 })
 export class FamilyController {
   constructor(private readonly familyService: FamilyService) {}
+
+  @Post('join')
+  async joinFamilyByParentEmail(userId: string, parentEmail: string) {
+    if (!userId || !parentEmail) throw new Error('Invalid input');
+    return await this.familyService.joinFamilyByParentEmail(
+      userId,
+      parentEmail,
+    );
+  }
+
+  @Post('remove')
+  async removeUserFromFamily(userId: string, parentId: string) {
+    if (!userId || !parentId) throw new Error('Invalid input');
+    return await this.familyService.removeUserFromFamily(userId, parentId);
+  }
 }
